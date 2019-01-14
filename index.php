eab05674733e2d3f722dead1a8fff28c86456606
Brandon Huffman <bt_huffman@msn.com>
2019-01-14 11:52:55 -0700

    Created index html and php file.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Basic Demo of Project Creation</title>
+	</head>
+	<body>
+		<h1>Hello Earth! Love Deep Dive Cohort 23</h1>
+	</body>
+</html>
